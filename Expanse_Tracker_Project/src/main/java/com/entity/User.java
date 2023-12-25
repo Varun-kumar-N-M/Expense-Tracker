879f2c2f3e336e6ff55c8f7bc3de2bf620b81077
Varun kumar N M <kumarnmvarun@gmail.com>
2023-12-25 14:49:08 +0530

    Final commit

---
+++ Expanse_Tracker_Project/src/main/java/com/entity/User.java
@@ -0,0 +1,70 @@
+package com.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="user_details")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	@Column(name="fullname")
+	private String fullName;
+	private String email;
+	private String password;
+	private String about;
+	public User() {
+		
+	}
+	public User(String fullName, String email, String password, String about) {
+		super();
+		this.fullName = fullName;
+		this.email = email;
+		this.password = password;
+		this.about = about;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getAbout() {
+		return about;
+	}
+	public void setAbout(String about) {
+		this.about = about;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", fullName=" + fullName + ", email=" + email + ", password=" + password + ", about="
+				+ about + "]";
+	}
+	
+	
+
+}
